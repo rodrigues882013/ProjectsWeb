e251e6535f3ef9d2516990c8ecee810a74fc4353
rodrigues882013 <300588fe>
2013-02-23 17:40:58 -0300

    Update views

--- controllers/default.py
+++ controllers/default.py
@@ -18,7 +18,7 @@ def index():
     if you need a simple wiki simple replace the two lines below with:
     return auth.wiki()
     """
-    response.flash = T("Welcome to web2py!")
+    response.flash = T("Bem vindo")
     return dict(message=T('Hello World'))
 
 
